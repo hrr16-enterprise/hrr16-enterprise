07d9e12c03ec55958217091b6c7f5312a735562a
Chris Weaver <cmagweaver@gmail.com>
2016-06-17 15:41:22 -0700

    (style) Add new Button component

---
+++ client/components/Button.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import * as actions from '../actions/index.js';
+
+export default class Button extends Component {
+  
+  render() {
+    return (
+      <div className="button">
+        <button>"The World in a minute"</button>
+      </div>
+    );
+  }
+}
