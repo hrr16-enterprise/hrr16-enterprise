e239a7efee524761b4d174eed59fb77fb4b226c2
gschrock <gabrielschrock@gmail.com>
2016-06-17 12:25:46 -0700

    (feat) Add navbar component

---
+++ client/components/NavBar.js
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes } from 'react';
+import * as actions from '../actions/index.js';
+
+export default class NavBar extends Component {
+  
+  render() {
+    return (
+      <div className="container">
+        This is going to be a nav bar container!
+      </div>
+    );
+  }
+}
