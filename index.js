b4f22d28a7db47a24eab8c6e52560c5805130e16
mDalessandro <mattd942@gmail.com>
2016-06-16 21:54:06 -0400

    (refactor) Convert another function to es6

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = require('./server/app.js');
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, function(){
+app.listen(port, () => {
   console.log(`server is now listening on port ${port}`);
 });
