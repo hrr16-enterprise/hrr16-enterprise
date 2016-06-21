b31c7b9c95a16673654096d7880c7f3ca441c4b7
mDalessandro <mattd942@gmail.com>
2016-06-21 14:42:08 -0400

    (feat) Add navbar container

---
+++ client/containers/NavBarContainer.js
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+import * as actions from '../actions/index.js';
+import NavBarComponent from '../components/NavBar.js';
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    // Add functions to dispatch login/logout actions
+  };
+};
+
+const mapStateToProps = (state) => {
+  return {
+    auth: state.auth,
+    ui: state.ui
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBarComponent);
