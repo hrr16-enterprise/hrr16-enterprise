5882e7ef386bae4a19a69a7764c30d04a0ccc8cc
mDalessandro <mattd942@gmail.com>
2016-06-23 13:06:21 -0400

    (fix) Swap key in flickr url to a variable

--- server/routes/results/results.js
+++ server/routes/results/results.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const Promise = require('bluebird');
-const helper = require('../../services/helper.js')
+const helper = require('../../services/helper.js');
+const private = require('../../private/private.js');
 
 //flickr request handlers
 router.get('/flickr', (req, res) => {
-  const flickrUrl = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=c47ece224080058910137d84a24cfe94&has_geo=&format=json&nojsoncallback=1';
+  const flickrUrl = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + private.FLICKR_KEY + '&has_geo=&format=json&nojsoncallback=1';
   return helper.getHelper(flickrUrl)
   .then((response) => {
     res.send(response);
