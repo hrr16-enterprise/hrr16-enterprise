31e6de6cf9277386beae9160227c575dce477d82
Chris Weaver <cmagweaver@gmail.com>
2016-06-22 22:56:52 -0700

    (feat) Add Flicker and Reddit call to FetchButton

--- client/components/FetchButton.js
+++ client/components/FetchButton.js
@@ -5,12 +5,11 @@ export default class FetchButton extends Component {
   constructor(props) {
     super(props);
   }
-  
 
   render() {
     return (
       <div className="button">
-        <button onTouchTap={this.props.fetchReddit}>"The World in a minute"</button>
+        <button onTouchTap={function(){this.props.fetchFlickr(); this.props.fetchReddit();}.bind(this) }>"The World in a minute"</button>
       </div>
     );
   }
