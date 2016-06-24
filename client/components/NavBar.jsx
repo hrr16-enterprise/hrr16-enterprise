69abfe21404fdd07b0c3363b5be5f2c4174fec65
Chris Weaver <cmagweaver@gmail.com>
2016-06-23 21:02:54 -0700

    (chore) Fix lintering errors and naming

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Login from '../containers/LoginContainer';
+import Auth from '../containers/AuthContainer';
+
 
 export const NavBar = ({ children }) => (
   <div>
