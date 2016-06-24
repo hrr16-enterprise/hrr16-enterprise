1f6cb340eb717275baa0a3a1c97de9dffe5f6d45
gschrock <gabrielschrock@gmail.com>
2016-06-23 21:24:42 -0700

    (feat) remove flickrgeo url add geo response to flickrurl

--- server/routes/results/results.js
+++ server/routes/results/results.js
@@ -4,11 +4,9 @@ const Promise = require('bluebird');
 const helper = require('../../services/helper.js');
 const private = require('../../private/private.js');
 
-const FLICKRGEO_URL = 'https://api.flickr.com/services/rest/?method=flickr.photos.geo.getLocation&api_key=18d27003cab88cde9cfbbb2c7a587a33&photo_id=&format=json&nojsoncallback=1';
-
 //flickr request handlers
 router.get('/flickr', (req, res) => {
-  const flickrUrl = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + private.FLICKR_KEY + '&has_geo=&format=json&nojsoncallback=1';
+  const flickrUrl = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + private.FLICKR_KEY + '&safe_search=1&content_type=1&has_geo=1&extras=geo&per_page=10&format=json&nojsoncallback=1';
   return helper.getHelper(flickrUrl)
   .then((response) => {
     res.send(response);
@@ -22,7 +20,7 @@ router.get('/flickr', (req, res) => {
 //reddit request handlers
 router.get('/reddit', (req, res) => {
   const redditUrl = 'https://www.reddit.com/new.json?sort=new?';
-  return helper.getHeler(redditUrl)
+  return helper.getHelper(redditUrl)
   .then((response) => {
     res.send(response);
   })
