cefb2de6750d0d8fdc030f1234cb493a228b036a
Brendan Pelletier <bdpellet@gmail.com>
2016-06-23 21:34:02 -0400

    (chore) Merge latest PR conflicts

--- client/containers/GlobeContainer.js
+++ client/containers/GlobeContainer.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import * as actions from '../actions/index.js';
-import GlobeComponent from '../components/Globe';
+import GlobeComponent from '../components/Globe.jsx';
 
 const mapDispatchToProps = (dispatch) => {
   return {
