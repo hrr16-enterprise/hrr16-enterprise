7fd103dfcf97181a448f564e0c6a0bd314f1972b
mDalessandro <mattd942@gmail.com>
2016-06-28 18:15:32 -0400

    (chore) Remove unused import line from corelayout component

--- client/components/CoreLayout.jsx
+++ client/components/CoreLayout.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import NavBar from '../components/NavBar.jsx';
 import FetchButton from '../containers/FetchButtonContainer';
 import ResultContainer from '../containers/ResultContainer';
-import YoutubeContainer from '../containers/YoutubeContainer';
 import Globe from '../containers/GlobeContainer';
 
 export const CoreLayout = ({ children }) => (
