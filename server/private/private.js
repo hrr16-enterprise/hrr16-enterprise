09a1f3a98661657ec8e2317577d81d941e32d110
Chris Weaver <cmagweaver@gmail.com>
2016-06-28 17:58:54 -0700

    (chore) Code Clean up for YELP API

--- server/private/private.js
+++ server/private/private.js
@@ -1,6 +1,6 @@
 const FLICKR_KEY = process.env.FLICKR_KEY;
 const NYTIMES_KEY = process.env.NYTIMES_KEY;
-const YOUTUBE_KEY = process.env.YOUTUBE_KEY;
+const YOUTUBE_KEY = process.env.YOUTUBE_DATA;
 
 //=======================
 // Authentication Keys
@@ -10,14 +10,15 @@ const Yelp = require('yelp');
 
 // Yelp
 const YELP_KEY = new Yelp({
-  consumer_key: 'N-QdzXYBf25HV-YhRQlfHQ',
-  consumer_secret: 'iQ1jEHYgmvlgaI1UBvKG7ZceRQA',
-  token: 'ntMl9EZxwCfubY9YAStJrBjwlWmDvFS8',
-  token_secret: '0BuTKkSCDDlSYpHW4P8zv91PcB8'
+  consumer_key: process.env.YELP_CONSUMER_KEY,
+  consumer_secret: process.env.YELP_CONSUMER_SECRET,
+  token: process.env.YELP_TOKEN,
+  token_secret: process.env.YELP_TOKEN_SECRET
 });
 
 module.exports = {
   FLICKR_KEY,
   NYTIMES_KEY,
-  YOUTUBE_KEY
+  YOUTUBE_KEY,
+  YELP_KEY
 };
