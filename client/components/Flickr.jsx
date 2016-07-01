0d51b4493bbea96e2f8467af90e5a1f85668bb5b
Chris Weaver <cmagweaver@gmail.com>
2016-07-01 12:11:00 -0700

    (feat) Add markers to all Flickr Api calls (#153)
    * (feat) Add markers to all Flickr Api calls
    
    * (fix) Remove console.log and comment in Flickr.jsx

--- client/components/Flickr.jsx
+++ client/components/Flickr.jsx
@@ -7,7 +7,7 @@ export default class Flickr extends Component {
 
   componentWillReceiveProps(object) {
     if (this.props.flickr.result.data) {
-      this.props.pingGlobe(this.props.flickr.globe, this.props.flickr.result.data.photos.photo[0].latitude, this.props.flickr.result.data.photos.photo[0].longitude);
+      this.props.pingGlobe(this.props.flickr.globe, this.props.flickr.result.data.photos.photo);
     }
   }
   
