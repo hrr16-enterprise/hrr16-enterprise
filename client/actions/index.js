27fc3d37dd766c63b99ca4f20c99cdc7de890389
Brendan <bdpellet@gmail.com>
2016-07-06 04:58:50 -0400

    (feat) Add functionality for marker colors

--- client/actions/index.js
+++ client/actions/index.js
@@ -182,6 +182,9 @@ export const pingGlobe = (html, globe, latitude, longitude, width, height) => {
     marker.lat = latitude;
     marker.lng = longitude;
     marker.addTo(globe).bindPopup(html, {maxWidth: width, maxHeight: height, closeButton: true});
+    // TODO: Need function here to identify which api data service and assign it a color to pass in the line below
+    var testColors = ['blue','pink','yellow','red','green']
+    marker.element.firstChild.style.backgroundColor = testColors[Math.floor(Math.random() * testColors.length)];
     markers.push(marker);
   }
   return {
