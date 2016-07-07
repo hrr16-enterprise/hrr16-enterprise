9c4a016656c7dc72fcfa2bbc3440ceb34f143820
Chris Weaver <cmagweaver@gmail.com>
2016-07-06 23:49:26 -0700

    (chore) Enable production mode for database

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,5 @@
 var config      = require('./knexfile.js');  
-var env         = 'development';  
+var env         = 'production';  
 var knex        = require('knex')(config[env]);
 
 module.exports = knex;
