71c867f99483d63cf5f2afa064dce795f71a6b91
gschrock <gabrielschrock@gmail.com>
2016-07-06 20:53:25 -0700

    (style) Update styling for all components, add hover response on fa icons

--- client/components/NYTimes.jsx
+++ client/components/NYTimes.jsx
@@ -10,10 +10,11 @@ export default class NYTimes extends Component {
       
       const NYTimesData = this.props.nytimes.result.data
       
-        this.props.pingGlobe("<div class='card' class='nyTimesContainer'><div class='nyTimesLogo'><img class='nyTimesImg' src='https://upload.wikimedia.org/wikipedia/commons/7/77/The_New_York_Times_logo.png'><i class='fa fa-heart fa-2x' id='nyTimesHeart' aria-hidden='true'></i></div>" +  
+        this.props.pingGlobe("<div class='nyTimesContainer'><div class='nyTimesLogo'><img class='nyTimesImg' src='https://upload.wikimedia.org/wikipedia/commons/7/77/The_New_York_Times_logo.png'></div>" +  
           "<a href=" + "'" + NYTimesData.results[0].article_list.results[0].url + "'" + "target='_blank'" + ">" + 
           NYTimesData.results[0].article_list.results[0].body + 
-          "</a></div>",
+          "</a></div>" +
+          "<i class='fa fa-heart fa-2x' id='nyTimesHeart' aria-hidden='true'></i>",
         this.props.nytimes.globe,
         NYTimesData.results[0].geocodes[0].latitude,
         NYTimesData.results[0].geocodes[0].longitude,
--- client/components/Yelp.jsx
+++ client/components/Yelp.jsx
@@ -12,26 +12,28 @@ export default class Yelp extends Component {
       const YelpData = this.props.yelp.result.data;
       
        for(let i = 0; i < YelpData.length; i++){
-         this.props.pingGlobe("<div class='card' class='yelpContainer'>" + 
+         this.props.pingGlobe("<div class='yelpContainer'>" + 
           "<div class='yelpLogo'>" + 
           "<img class='yelpImg' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Yelp_Logo.svg/1024px-Yelp_Logo.svg.png'>" + 
-          "<iframe name='hiddenFrame' width='0' height='0' border='0' style='display: none;'></iframe>" +
-          "<form action = '/results/user/data' method = 'post' target='hiddenFrame'>" +
-          "<input name = 'userID' type='hidden' value=" + JSON.parse(localStorage.getItem('profile')).clientID + ">" +
-          "<input name = 'imageUrl' type='hidden' value=" + YelpData[i].image_url + ">" + 
-          "<button class='favoriteButton' name='url' value="+ YelpData[i].url +">" + 
-          "<i class='fa fa-heart fa-2x' id='yelpHeart' aria-hidden='true'>" + 
-          "</i></button></form></div>" + 
+          "<iframe name='hiddenFrame' width='0' height='0' border='0' style='display: none;'></iframe>" + 
 
           "<a href=" + "'" + YelpData[i].url + "'" + "target='_blank'" + ">" + YelpData[i].name + "</a>" + 
           "<div><img src =" + YelpData[i].image_url +"></div>" + 
           "<div><img src =" + YelpData[i].rating_img_url +"></div>" +
           "<div>" + YelpData[i].deals[0].options[0].title + "</div>" +  
 
-          "" + (Math.floor(YelpData[i].distance)*0.0006).toFixed(2) + "" + " mi away</div>",
+          "" + (Math.floor(YelpData[i].distance)*0.0006).toFixed(2) + "" + " mi away</div>" +
+          "<form action = '/results/user/data' method = 'post' target='hiddenFrame'>" +
+          "<input name = 'userID' type='hidden' value=" + JSON.parse(localStorage.getItem('profile')).clientID + ">" +
+          "<input name = 'imageUrl' type='hidden' value=" + YelpData[i].image_url + ">" + 
+          "<button class='favoriteButton' name='url' value="+ YelpData[i].url +">" + 
+          "<i class='fa fa-heart fa-2x' id='yelpHeart' aria-hidden='true'>" + 
+          "</i></button></form></div>",
           this.props.yelp.globe,
           YelpData[i].location.coordinate.latitude, 
-          YelpData[i].location.coordinate.longitude
+          YelpData[i].location.coordinate.longitude,
+          200,
+          200
         );
       }       
     }    
